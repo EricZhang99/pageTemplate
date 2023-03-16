fb9bdb8311b6f3bbc84a35510dd805037006ab29
EricZhang99 <760411543@qq.com>
2023-03-16 19:10:46 +0800

    脚手架-工作室设置

---
+++ src/components/UserCardView.vue
@@ -0,0 +1,26 @@
+<script setup>
+// showCard = false;
+showCard = false;
+function log() {
+    console.log("click")
+    showCard = !showCard
+}
+</script>
+
+<template>
+    <div class="block" @click=log>
+        <el-avatar :size="50" :src="circleUrl" />
+    </div>
+    <div v-show=showCard class="userCard">
+        hello
+    </div>
+</template>
+
+<style scoped>
+.userCard {
+    height: 20vh;
+    width: 20VH;
+    position: relative;
+    color: blue;
+}
+</style>
